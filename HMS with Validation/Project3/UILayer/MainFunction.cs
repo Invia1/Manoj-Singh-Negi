087c0151a274b443a103f4a5ae1d0228096980bf
Invia1 <132645377+Invia1@users.noreply.github.com>
2023-05-29 15:05:55 +0530

    Add files via upload

---
+++ HMS with Validation/Project3/UILayer/MainFunction.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Project3.Model;
+using Project3.service;
+using Project3.ServiceControll;
+
+namespace Project3.UILayer
+{
+    internal class MainFunction
+    {  
+        public static void Main(string[] args)
+        {
+            string username = "admin";
+            string password = "admin";
+           
+            Interface1 inter = new PatientDetails();
+            MainFunction fun = new MainFunction();
+            int regid;
+            string consulting;
+            Patient patient1 = new Patient();
+            Boolean flag = true;
+            Boolean flag2 = true;
+
+            while (flag2)
+            {
+                Console.WriteLine("enter username");
+                string uname = Console.ReadLine();
+
+                Console.WriteLine("enter password");
+                string upass = Console.ReadLine();
+
+                if (uname.Equals(username) && upass.Equals(password))
+                {
+                    flag2 = false;
+
+                    while (flag)
+                    {
+                        Console.WriteLine("press 1 for adding new patient");
+                        Console.WriteLine("press 2 for viewing the list of patient");
+                        Console.WriteLine("press 3 for update");
+                        Console.WriteLine("press 4 for delete");
+                        Console.WriteLine("press 5 for searching patient of General Medicine");
+                        Console.WriteLine("press 6 for searching patient of ortho");
+                        Console.WriteLine("press 7 for searching patient of dental");
+                        Console.WriteLine("press 8 for exit");
+
+                        Console.WriteLine("enter your choice :");
+                        int choice = Convert.ToInt32(Console.ReadLine());
+                        Boolean flag3 = true;
+                       
+                        switch (choice)
+                        {
+                            case 1:
+                                while (flag3)
+                                {
+                                    try
+                                    {
+                                        Console.WriteLine("<<<<----------Enter Patient Details------------>>>>");
+                                        Patient patient = new Patient();
+                                        Console.WriteLine("enter Patient registration id : ");
+                                        patient.RegistrationId = Convert.ToInt32(Console.ReadLine());
+                                       
+                                        Console.WriteLine("enter patient First Name : ");
+                                        patient.PatientFirstName = Console.ReadLine();
+                                        
+                                      
+                                        Console.WriteLine("enter patient Last Name ");
+                                        patient.PatientLastName = Console.ReadLine();
+                                       
+
+                                        Console.WriteLine("enter patient Gender(M/F) ");
+                                        patient.PatientGender = Console.ReadLine();
+
+                                        Console.WriteLine("enter Patient phone number : ");
+                                        patient.PatientPhoneNumber = Console.ReadLine();
+
+                                        Console.WriteLine("enter Patient age : ");
+                                        patient.PatientAge = Convert.ToInt32(Console.ReadLine());
+
+                                        Console.WriteLine("enter Patient aadhar number : ");
+                                        patient.PatienAAdhar = Console.ReadLine();
+
+                                        Console.WriteLine("enter patient address : ");
+                                        patient.PatientAddress = Console.ReadLine();
+
+                                        Console.WriteLine("enter patient consultant choice:");
+                                        Console.WriteLine("options are");
+                                        Console.WriteLine("1- General Medicine");
+                                        Console.WriteLine("2- Orthopaedics");
+                                        Console.WriteLine("3- Dental");
+                                        Console.WriteLine("4 exit");
+                                        Console.WriteLine("enter your choice");
+                                        int choice1 = Convert.ToInt32(Console.ReadLine());
+                                        switch (choice1)
+                                        {
+                                            case 1:
+                                                patient.PatientConsult = "General Medicine";
+                                                break;
+
+                                            case 2:
+                                                patient.PatientConsult = "Orthopaedics";
+                                                break;
+
+                                            case 3:
+                                                patient.PatientConsult = "Dental";
+                                                break;
+
+                                            case 4:
+                                                flag2 = false;
+                                                break;
+
+                                            default:
+                                                Console.WriteLine("please enter a valid choice");
+                                                break;
+                                        }
+                                        inter.createPatient(patient);
+                                        Console.WriteLine("press 'Enter' for exit");
+                                        string str = Console.ReadLine();
+                                        if (str == "")
+                                        {
+                                            flag3 = false;
+                                        }
+                                    }
+                                    
+                                    catch(Exception e)
+                                    {
+                                        Console.WriteLine(e.Message); 
+                                    }
+                                }
+                                break;
+                            case 2:
+                                Console.WriteLine("************************************");
+                                Console.WriteLine("your data in the list are: ");
+                                List<Patient> patientlist = inter.getPatients();
+                                foreach (var items in patientlist)
+                                { 
+                                    Console.WriteLine(items.RegistrationId +" |"+items.PatientFirstName+"\t|"+items.PatientLastName+"\t|"+items.PatientGender+"|"+items.PatientPhoneNumber+"|"+items.PatientAge+" |"+items.PatienAAdhar+"|"+items.PatientAddress+"\t|"+items.PatientConsult);
+                     
+                                }
+                                Console.WriteLine("*******************************************************************************");
+                                break;
+
+                            case 3:
+                                Console.WriteLine("to update the patient enter the registration id ");
+                                regid = Convert.ToInt32(Console.ReadLine());
+                                inter.updatePatient(regid);
+                                Console.WriteLine("******************************");
+                                break;
+
+                            case 4:
+                                Console.WriteLine("to delete a patient enter the registration id");
+                                regid = Convert.ToInt32(Console.ReadLine());
+                                inter.DeletePatient(regid);
+                                Console.WriteLine("***************************************");
+                                break;
+
+                            case 5:
+                                consulting = "General Medicine";
+                                inter.searchGMPatient(consulting);
+                                Console.WriteLine("***************************************");
+                                break;
+
+                            case 6:
+                                consulting = "Orthopaedics";
+                                inter.searchOrthoPatient(consulting);
+                                Console.WriteLine("***************************************");
+                                break;
+
+                            case 7:
+
+                                consulting = "Dental";
+                                inter.searchDentalPatient(consulting);
+                                Console.WriteLine("**************************************");
+                                break;
+
+                            case 8:
+                                flag = false;
+                                break;
+
+
+                            default:
+                                Console.WriteLine("please enter a valid");
+                                break;
+                        }
+
+                    }
+                }
+                else if(!uname.Equals(username))
+                {
+                    Console.WriteLine("please enter valid username");
+                    Console.WriteLine();
+                }
+                else 
+                {
+                    Console.WriteLine("please enter valid password");
+                    Console.WriteLine();
+                }
+            }
+        }     
+
+    }
+}
---
+++ HMS with Validation/Project3/service/PatientDetails.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using Project3.Model;
+using Project3.ServiceControll;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project3.service
+{
+    internal class PatientDetails:Interface1
+    {
+        List<Patient> list = new List<Patient>();
+        public int createPatient(Patient patient)
+        {
+            int countR = 0;
+            int countAC = 0;
+            List<Patient> plist = getPatients();
+            foreach(Patient pat in plist)
+            {
+               if(pat.RegistrationId==patient.RegistrationId)
+                {
+                    countR++;
+                }
+                else if(pat.PatienAAdhar==patient.PatienAAdhar)
+                {
+                    countAC++;
+                }
+            }
+            if(countR>0)
+            {
+                Console.WriteLine("Sorryyyyyy...................");
+                Console.WriteLine("this Registration id is already registered....");
+            }
+            else if(countAC>0)
+            {
+                Console.WriteLine("Sorryyyyyy...................");
+                Console.WriteLine("this Aadhar number is already registered....");
+            }
+            else 
+            {
+                list.Add(patient);
+            }
+            return 1;
+        }
+        public List<Patient> getPatients()
+        {
+            return list;
+        }
+        public void updatePatient(int id)
+        {
+            List<Patient>patients = getPatients();
+            try
+            {
+                int count = 0;
+                foreach (Patient pat in patients)
+                {
+                    if(pat.RegistrationId.Equals(id))
+                    {
+                        Console.WriteLine("First Name");
+                        pat.PatientFirstName=Console.ReadLine();
+                        Console.WriteLine("Last Name ");
+                        pat.PatientLastName=Console.ReadLine();
+                        Console.WriteLine("Phone number");
+                        pat.PatientPhoneNumber=Console.ReadLine();
+                        Console.WriteLine("age");
+                        pat.PatientAge=Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("gender");
+                        pat.PatientGender=Console.ReadLine();
+                        Console.WriteLine("Aadhar card Number");
+                        pat.PatienAAdhar=Console.ReadLine();
+                        Console.WriteLine("Address");
+                        pat.PatientAddress=Console.ReadLine();
+
+                        Console.WriteLine("enter patient consultant choice:");
+                        Console.WriteLine("options are");
+                        Console.WriteLine("1- General Medicine");
+                        Console.WriteLine("2- Orthopaedics");
+                        Console.WriteLine("3- Dental");
+                        Console.WriteLine("enter your choice");
+                        int choice1 = Convert.ToInt32(Console.ReadLine());
+                        switch (choice1)
+                        {
+                            case 1:
+                                pat.PatientConsult = "General Medicine";
+                                break;
+
+                            case 2:
+                                pat.PatientConsult = "Orthopaedics";
+                                break;
+
+                            case 3:
+                                pat.PatientConsult = "Dental";
+                                break;
+
+                            default:
+                                Console.WriteLine("please enter a valid choice");
+                                break;
+                        }
+
+                        count++;
+                        
+                    }
+                }
+                if(count== 0) {
+                    Console.WriteLine("patient data is not available");
+                }
+
+            }
+            catch(Exception e) { 
+                Console.WriteLine(e.Message);
+            
+            }
+        }
+        public void DeletePatient(int id)
+        {
+            List<Patient> patients = getPatients();
+            
+               foreach(Patient pat in patients)
+                {
+                    if(pat.RegistrationId.Equals(id))
+                    {
+                        patients.Remove(pat);
+                    }
+                }
+
+        }
+        public void searchGMPatient(string type)
+        {
+            List<Patient> patients = getPatients();
+            if (patients.Count == 0)
+            {
+                Console.WriteLine("list is empty");
+            }
+            try
+            {
+                foreach (Patient pat in patients)
+                {
+                    if (pat.PatientConsult.Equals(type))
+                    {
+                        Console.WriteLine("registration Id :"+pat.RegistrationId);
+                        Console.WriteLine("First Name      :"+pat.PatientFirstName);
+                        Console.WriteLine("Last Name       :"+pat.PatientLastName);
+                        Console.WriteLine("Gender          :"+pat.PatientGender);
+                        Console.WriteLine("Age             :"+pat.PatientAge);
+                        Console.WriteLine("Address         :"+pat.PatientAddress);
+                        Console.WriteLine("Aadhar          :"+pat.PatienAAdhar);
+                        Console.WriteLine("Phone Number    :"+pat.PatientPhoneNumber);
+                        Console.WriteLine("consult with    :"+pat.PatientConsult);
+                        Console.WriteLine("***********************");
+
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+       public  void searchOrthoPatient(string type)
+        {
+            List<Patient> patients = getPatients();
+            if (patients.Count == 0)
+            {
+                Console.WriteLine("list is empty");
+            }
+            try
+            {
+                foreach (Patient pat in patients)
+                {
+                    if (pat.PatientConsult.Equals(type))
+                    {
+                        Console.WriteLine("registration Id :" + pat.RegistrationId);
+                        Console.WriteLine("First Name      :" + pat.PatientFirstName);
+                        Console.WriteLine("Last Name       :" + pat.PatientLastName);
+                        Console.WriteLine("Gender          :" + pat.PatientGender);
+                        Console.WriteLine("Age             :" + pat.PatientAge);
+                        Console.WriteLine("Address         :" + pat.PatientAddress);
+                        Console.WriteLine("Aadhar          :" + pat.PatienAAdhar);
+                        Console.WriteLine("Phone Number    :" + pat.PatientPhoneNumber);
+                        Console.WriteLine("consult with    :" + pat.PatientConsult);
+                        Console.WriteLine("***********************");
+
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+       public  void searchDentalPatient(string type)
+        {
+            List<Patient> patients = getPatients();
+            if(patients.Count == 0) {
+                Console.WriteLine("list is empty");
+            }
+            try
+            {
+                foreach (Patient pat in patients)
+                {
+                    if (pat.PatientConsult.Equals(type))
+                    {
+                        Console.WriteLine("registration Id :" + pat.RegistrationId);
+                        Console.WriteLine("First Name      :" + pat.PatientFirstName);
+                        Console.WriteLine("Last Name       :" + pat.PatientLastName);
+                        Console.WriteLine("Gender          :" + pat.PatientGender);
+                        Console.WriteLine("Age             :" + pat.PatientAge);
+                        Console.WriteLine("Address         :" + pat.PatientAddress);
+                        Console.WriteLine("Aadhar          :" + pat.PatienAAdhar);
+                        Console.WriteLine("Phone Number    :" + pat.PatientPhoneNumber);
+                        Console.WriteLine("consult with    :" + pat.PatientConsult);
+                        Console.WriteLine("***********************");
+
+                    }    
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+    }
+}
